f7a97a2768462740ba974bea3dead1dfdaa9ad33
Cédric B <cedricbardaine@gmail.com>
2020-10-28 15:05:18 +0100

    deploy

---
+++ precache-manifest.2b45539622fefa2491e91cf892d603b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0853b20202b7d8400a24",
+    "url": "/Chronotick/css/app.19562d92.css"
+  },
+  {
+    "revision": "21dfa3fe8041ee5efa58",
+    "url": "/Chronotick/css/chunk-vendors.d1919a06.css"
+  },
+  {
+    "revision": "a2b6d32b6a8254163fc4dffeadcb61fb",
+    "url": "/Chronotick/index.html"
+  },
+  {
+    "revision": "0853b20202b7d8400a24",
+    "url": "/Chronotick/js/app.8aec5b0c.js"
+  },
+  {
+    "revision": "21dfa3fe8041ee5efa58",
+    "url": "/Chronotick/js/chunk-vendors.1b3b033c.js"
+  },
+  {
+    "revision": "caeb9adfd1b747e9ce1313d5100e1a17",
+    "url": "/Chronotick/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Chronotick/robots.txt"
+  }
+]);
